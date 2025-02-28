cdd7b33580760dc5d7688a65d8ebaf7be64738f5
Diego <diegocosta.contato2021@gmail.com>
2025-02-28 09:45:40 -0300

    [feat]-Aplicando bootstrap, css e funcoes js.

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,11 @@
+function showModal(modalId)
+{
+    console.log(modalId)
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function hideModal(modalId)
+{
+    console.log(modalId)
+    document.getElementById(modalId).style.display = 'none';
+}
